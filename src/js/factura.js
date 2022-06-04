78b751734002c750a1d32ce24d4cd0b6e87714d9
Jaime Puente <jpcontreras28@gmail.com>
2022-06-03 20:49:03 -0400

    ADAPTADA A LA NUEVA VERSION CANTIDAD-DESPACHO-EMPRESA

--- src/js/factura.js
+++ src/js/factura.js
@@ -11,6 +11,9 @@ const facturaPago = document.querySelector(".factura__pago");
 const montoSubtotal = document.querySelector(".monto__subtotal");
 const montoIva = document.querySelector(".monto__iva");
 const montoTotal = document.querySelector(".monto__total");
+const empresaDetalles = document.querySelectorAll(".empresa__detalles");
+const empresaDetalles2 = document.querySelectorAll(".empresa__detalles2");
+const empresaArticulos = document.querySelectorAll(".empresa__articulos");
 
 const random7 = Math.round(Math.random() * 9999999);
 const date = new Date();
@@ -20,39 +23,60 @@ const año = date.getFullYear();
 const iva = 0.16;
 const total = ()=>{
     let precio = 0;
-    cd.articulos.forEach((el) => {precio += el.precio});
+    cd.articulos.forEach((el) => {precio += el.articulo.precio*el.cantidad});
     return precio;
 };
 
 //FUNCION PARA CREAR PARRAFOS, EN ALIGN: 1 PARA LEFT, 2 PARA RIGHT
-function cp(value,align,padre,id=""){
+function cp(value,align,padre,id="",despues=null){
     let alinear = "align-left";
-    if(!(align == 1)){
+    if((align == 2)){
         alinear = "align-right";
+    } else if(align == 1){
+        alinear;
+    } 
+    else{
+        alinear = "align-center";
     }
-    const el = crearElemento("p",value,alinear,padre,id);
+    const el = crearElemento("p",value,alinear,padre,id,despues);
     return el;
 }
 
-cp(cd.nombre,1,destinatarioNombre);
-cp(cd.id,1, destinatarioNombre);
-cp(cd.direccion,1, destinatarioDatos);
-cp(`${cd.ciudad} (${cd.pais})`,1, destinatarioDatos);
-cp(cd.email,1, destinatarioDatos);
-cp(`#${random7}`,2, facturaNumero);
-cp(`${dia}/${mes}/${año}`,2, facturaFecha);
-cp(`${dia}/${mes + 1}/${año}`,2, facturaVence);
-cp(`$${total()}`,1, facturaTotal);
-cp(`${cd.formaPago.name}: ${cd.formaPago.entidad}`,1,facturaPago);
-cp(`$${total()}`,2, montoSubtotal).classList.add("caja","no-margin");
-cp(`$${(total() * iva).toFixed(2)}`,2, montoIva).classList.add("caja","no-margin");
-cp(`$${(total() * (iva + 1)).toFixed(2)}`,2,montoTotal).classList.add("caja", "no-margin");
+const factNombre = cp(cd.nombre,1,destinatarioNombre);
+const factId = cp(cd.id,1, destinatarioNombre);
+const factDireccion = cp(cd.direccion,1, destinatarioDatos);
+const factUbicacion = cp(`${cd.ciudad} (${cd.pais})`,1, destinatarioDatos);
+const factEmail = cp(cd.email,1, destinatarioDatos);
+const factNum = cp(`#${random7}`,2, facturaNumero);
+const factFecha = cp(`${dia}/${mes}/${año}`,2, facturaFecha);
+const factVence = cp(`${dia}/${mes + 1}/${año}`,2, facturaVence);
+const factTotal1 = cp(`$${(total() * (iva + 1)).toFixed(2)}`,1, facturaTotal);
+const factPago = cp(`${cd.formaPago.name}: ${cd.formaPago.entidad}`,1,facturaPago);
+const factSubtotal = cp(`$${total()}`,2, montoSubtotal).classList.add("caja","no-margin");
+const factIva = cp(`$${(total() * iva).toFixed(2)}`,2, montoIva).classList.add("caja","no-margin");
+const factTotal = cp(`$${(total() * (iva + 1)).toFixed(2)}`,2,montoTotal).classList.add("caja", "no-margin");
 
 cd.articulos.forEach((el, index) => {
     const div = crearElemento("div", "", "articulos__detalles", articulosContainer, index);
-    const p = cp(el.name,1, div).classList.add("caja","no-margin");
-    const pr = cp(`$${el.precio}`,2, div).classList.add("caja","no-margin");
+    const p = cp(`${el.cantidad}  -  ${el.articulo.name}`,1, div).classList.add("caja","no-margin");
+    const pr = cp(`$${el.articulo.precio*el.cantidad}`,2, div).classList.add("caja","no-margin");
 });
 
+empresaDetalles.forEach(el => {
+    cp(`#${random7}`,3,el);
+    cp(`${dia} / ${mes} / ${año}`,3,el);
+});
+
+empresaArticulos.forEach(el => {
+    cd.articulos.forEach((art) => {
+        cp(`${art.cantidad}  -  ${art.articulo.name} - $${art.articulo.precio*art.cantidad}`,1, el)
+        .classList.add("copia--articulo");
+    });
+    cp(`Total: $${(total() * (iva + 1)).toFixed(2)}`,1, el).classList.add("copia--total");
+});
+
+empresaDetalles2.forEach(el => {
+    cp(`${cd.formaPago.name}: ${cd.formaPago.entidad}`,3,el);
+});
 
 
